b95349e21f09d41747fa5cf3bb946c32ead8db34
wenhuidong <xiaodongdong258@outlook.com>
2022-02-16 10:08:35 +0800

    消除了No MyBatis mapper was found in '[xx.mapper]' package. 的警告

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,10 +1,13 @@
 package com.example.demo;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableTransactionManagement
+@MapperScan("com.example.demo.*.dao")
 public class DemoApplication {
 
 	public static void main(String[] args) {
