74ea429e235a5ea9c142e6d23a958e8655c41172
wenhuidong <xiaodongdong258@outlook.com>
2022-02-21 16:28:28 +0800

    增添了DocumentRecord用于交互，增加了控制器的post上传功能，为Documnent添加了备注

--- src/main/java/com/example/demo/doc/controller/DocumentController.java
+++ src/main/java/com/example/demo/doc/controller/DocumentController.java
@@ -1,9 +1,17 @@
 package com.example.demo.doc.controller;
 
+import com.example.demo.doc.entity.DocumentRecord;
 import com.example.demo.doc.service.DocumentService;
+import com.example.demo.interactive.RestResponse;
+import com.fasterxml.jackson.core.JacksonException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @RestController
 public class DocumentController {
@@ -16,4 +24,45 @@ public class DocumentController {
         this.documentService = documentService;
     }
 
+    //JSON jackson
+    ObjectMapper objectMapper = new ObjectMapper();
+
+    @PostMapping("/document/post")
+    public String postDocument(@RequestBody String JSONString) {
+        logger.info("postDocument!");
+        RestResponse restResponse = new RestResponse();
+        String result;
+        DocumentRecord record;
+        //将前端发来的JSON反序列化，转为DocumentRecord
+        try {
+            record = objectMapper.readValue(JSONString, DocumentRecord.class);
+        }catch (Exception e1){
+            //打出错误日志
+            logger.error("e1 postDocument JSON数据格式有误，解析JSON数据失败: ");
+            e1.printStackTrace();
+            //通过接口返回，服务器无法根据客户端请求的内容特性完成请求(Not Acceptable)
+            restResponse.setCode(406);
+            restResponse.setMessage("JSON数据格式有误，解析JSON数据失败!");
+            try {
+                result = objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(restResponse);
+            }catch (Exception e2){
+                logger.error("e2 postDocument 后台回应转JSON数据失败: ");
+                e2.printStackTrace();
+                return "后台回应转JSON数据失败!";
+            }
+            return result;
+        }
+        //成功获取到Record,注意service层是否有异常！
+        //documentService.add(record);
+        restResponse.setCode(200);
+        restResponse.setMessage("上传数据成功！");
+        try {
+            result = objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(restResponse);
+        }catch (Exception e3){
+            logger.error("e3 postDocument 后台回应转JSON数据失败: ");
+            e3.printStackTrace();
+            return "后台回应转JSON数据失败!";
+        }
+        return result;
+    }
 }
--- src/main/java/com/example/demo/doc/entity/DocumentRecord.java
+++ src/main/java/com/example/demo/doc/entity/DocumentRecord.java
@@ -1,79 +1,23 @@
 package com.example.demo.doc.entity;
 
-import java.util.Date;
-
-public class Document {
-    //操作id
-    private Integer id;
-
-    //文件id
-    private Integer documentId;
-
-    //版本id
-    private Integer versionId;
-
+public class DocumentRecord {
+    //只保存前端发来的数据，Document根据需要从DocumentRecord中提取
     //关联历史文件id
     private String historyDocumentId;
-
     //文件名称
     private String documentName;
-
     //文件密等
     private String documentSecretLevel;
-
     //文件编号
     private String documentReleaseNumber;
-
     //发布时间
     private String documentReleaseTime;
-
     //用户姓名
     private String operatorName;
-
-    //操作时间
-    private Date operateTime;
-
     //操作说明
     private String operateRemarks;
-
-    //操作类型
-    private String operateType;
-
-    private String categoryValue1;
-
-    private String categoryValue2;
-
-    private String categoryValue3;
-
     //文件正文
     private String documentText;
-
-    //图片链接
-    private String pictureLink;
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public Integer getDocumentId() {
-        return documentId;
-    }
-
-    public void setDocumentId(Integer documentId) {
-        this.documentId = documentId;
-    }
-
-    public Integer getVersionId() {
-        return versionId;
-    }
-
-    public void setVersionId(Integer versionId) {
-        this.versionId = versionId;
-    }
 
     public String getHistoryDocumentId() {
         return historyDocumentId;
@@ -123,14 +67,6 @@ public class Document {
         this.operatorName = operatorName == null ? null : operatorName.trim();
     }
 
-    public Date getOperateTime() {
-        return operateTime;
-    }
-
-    public void setOperateTime(Date operateTime) {
-        this.operateTime = operateTime;
-    }
-
     public String getOperateRemarks() {
         return operateRemarks;
     }
@@ -139,51 +75,11 @@ public class Document {
         this.operateRemarks = operateRemarks == null ? null : operateRemarks.trim();
     }
 
-    public String getOperateType() {
-        return operateType;
-    }
-
-    public void setOperateType(String operateType) {
-        this.operateType = operateType == null ? null : operateType.trim();
-    }
-
-    public String getCategoryValue1() {
-        return categoryValue1;
-    }
-
-    public void setCategoryValue1(String categoryValue1) {
-        this.categoryValue1 = categoryValue1 == null ? null : categoryValue1.trim();
-    }
-
-    public String getCategoryValue2() {
-        return categoryValue2;
-    }
-
-    public void setCategoryValue2(String categoryValue2) {
-        this.categoryValue2 = categoryValue2 == null ? null : categoryValue2.trim();
-    }
-
-    public String getCategoryValue3() {
-        return categoryValue3;
-    }
-
-    public void setCategoryValue3(String categoryValue3) {
-        this.categoryValue3 = categoryValue3 == null ? null : categoryValue3.trim();
-    }
-
     public String getDocumentText() {
         return documentText;
     }
 
     public void setDocumentText(String documentText) {
         this.documentText = documentText == null ? null : documentText.trim();
-    }
-
-    public String getPictureLink() {
-        return pictureLink;
-    }
-
-    public void setPictureLink(String pictureLink) {
-        this.pictureLink = pictureLink == null ? null : pictureLink.trim();
     }
 }
